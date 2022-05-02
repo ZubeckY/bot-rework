a70f425499aafd254049db19f659c57b4e9994f3
Oleg Zubkov <77145335+ZubeckY@users.noreply.github.com>
2022-05-02 21:17:38 +0300

    Add files via upload

---
+++ client/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="wrapper" outlined>
+    <header class="header">
+      <h4 class="header-title"></h4>
+    </header>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+  </v-card>
+</template>
+<script lang="ts">
+
+</script>
+<style>
+.wrapper {
+  max-width: 500px;
+}
+.header-title {
+
+}
+
+</style>
